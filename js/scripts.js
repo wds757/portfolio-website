7c9a0063c78c5d5bbe3d65af7bb066925718b441
wds757 <124187428+wds757@users.noreply.github.com>
2023-03-15 11:55:59 -0400

    Update scripts.js
    added Evans code - see tie ins to header and CSS

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ alert("Looking for an amazing employee? You've come to the right place!");
 
 console.log ("hello world")
 
+/*Menu toggle*/
+function menuToggle(x) {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
